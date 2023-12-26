34fb9c6831371d8e433e35c81cae59b3b7cae697
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2023-12-26 13:24:25 +0900

    Update 세로 읽기.java

--- algorithm/세로 읽기.java
+++ algorithm/세로 읽기.java
@@ -9,7 +9,7 @@ class Solution {
     public String solution(String myString, int m, int c) {
         return IntStream.range(0, myString.length())
             .filter(i -> i % m == c - 1)
-            .mapToObj(i -> String.valueOf(myString.charAt(i)))
+            .mapToObj(i -> String.valueOf(myString.charAt(i))) // m의 배수 중에서 c - 1과 일치하는 인덱스만 필터링합니다. 이것은 세로로 c번째 열에 해당하는 문자의 인덱스를 찾는 조건입니다.
             .collect(Collectors.joining());
     }
 }
