6db399a5b86dc44fbeaaafe7bcf68ae58820e24b
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2023-12-31 17:34:10 +0900

    Create 평균 구하기.java

---
+++ algorithm/평균 구하기.java
@@ -0,0 +1,13 @@
+/**
+  * 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+  */
+
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public double solution(int[] arr) {
+
+        return Arrays.stream(arr).average().getAsDouble();
+    }
+}
